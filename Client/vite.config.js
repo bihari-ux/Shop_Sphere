4a09b20142131a27e723bea277c2ada683c9f9c1
bihari-ux <biharikumarrawat123@gmail.com>
2026-06-14 13:17:53 +0530

    updated

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     build: {
-      outDir: '../Server/build',
+      outDir: 'build',
       emptyOutDir: true
     }
   }
